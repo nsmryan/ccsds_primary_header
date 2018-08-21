60be4ee8b6295c9123e166d43026e3bd89cada0a
nsmryan <nsmryan@gmail.com>
2018-08-21 08:46:02 -0400

    implemented primary header parser!

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,34 @@
+#[macro_use]
 pub mod types;
 use types::*;
+use nom::*;
 
-extern create nom;
+extern crate nom;
 
-/*
-named!(primary_header,
-  do_parse!(
-    version : ccsds_version,
-    packet_type : ccsds_packet_type,
-    sec_header_flag : ccsds_sec_header_flag,
-    apid : ccsds_apid,
-    seq_flag : ccsds_seq_flag,
-    seq : ccsds_seq,
-    len : ccsds_len,
+named!(ccsds_first_word<(u8, u8, u8, u16)>,
+       bits!(tuple!(take_bits!(u8,   3),     // version
+                    take_bits!(u8,   1),     // packet type
+                    take_bits!(u8,   1),     // secondary header
+                    take_bits!(u16, 11)))); // apid 
 
-    (PrimaryHeader::new(
-      {  version = version
-      , packet_type = packet_type
-      , sec_header_flag = sec_header_flag 
-      , apid = apid
-      , seq_flag = seq_flag
-      , seq = seq
-      , len = len
-      }))
-  )
-);
+named!(ccsds_seq_word<(u8, u16)>,
+       bits!(tuple!(take_bits!(u8, 2), take_bits!(u16, 14))));
 
+named!(ccsds_length_field<u16>, call!(be_u16));
+
+named!(primary_header<PrimaryHeader>,
+       do_parse!(
+         first_word : ccsds_first_word >>
+         seq_word : ccsds_seq_word     >>
+         len : ccsds_length_field      >>
+
+         (PrimaryHeader
+           { version : first_word.0
+           , packet_type : PacketType::from(first_word.1)
+           , sec_header_flag : SecondaryHeaderFlag::from(first_word.2)
+           , apid : first_word.3
+           , seq_flag : SeqFlag::from(seq_word.0)
+           , seq : seq_word.1
+           , len : len 
+           })));
 
--- src/types.rs
+++ src/types.rs
@@ -10,14 +10,36 @@ const CCSDS_MIN_LENGTH : usize = mem::size_of::<PrimaryHeader>() + 1;
 #[derive(Debug, PartialEq)]
 pub enum PacketType {
   Data,
-  Command
+  Command,
+  Unknown
 } 
+
+impl From<u8> for PacketType {
+    fn from(byte : u8) -> PacketType {
+        match byte {
+            0 => PacketType::Data,
+            1 => PacketType::Command,
+            _ => PacketType::Unknown
+        }
+    }
+}
 
 #[derive(Debug, PartialEq)]
 pub enum SecondaryHeaderFlag {
   NotPresent,
-  Present
+  Present,
+  Unknown
 } 
+
+impl From<u8> for SecondaryHeaderFlag {
+    fn from(byte : u8) -> SecondaryHeaderFlag {
+        match byte {
+            0 => SecondaryHeaderFlag::NotPresent,
+            1 => SecondaryHeaderFlag::Present,
+            _ => SecondaryHeaderFlag::Unknown
+        }
+    }
+}
 
 
 #[derive(Debug, PartialEq)]
@@ -25,7 +47,20 @@ pub enum SeqFlag {
   Continuation,
   FirstSegment,
   LastSegment,
-  Unsegmented
+  Unsegmented,
+  Unknown
+}
+
+impl From<u8> for SeqFlag {
+    fn from(byte : u8) -> SeqFlag {
+        match byte {
+            0 => SeqFlag::Continuation,
+            1 => SeqFlag::FirstSegment,
+            2 => SeqFlag::LastSegment,
+            3 => SeqFlag::Unsegmented,
+            _ => SeqFlag::Unknown
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
